8b6dcf3cc142d9ece52879848061b2164041ef57
spenc <spenc@LAPTOP-8LL6OQLE>
2017-06-06 11:52:27 -0600

    Add project files.

---
+++ Present Value Calculator/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Present_Value_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.howMuchLabel = new System.Windows.Forms.Label();
+            this.howLongLabel = new System.Windows.Forms.Label();
+            this.intRateLabel = new System.Windows.Forms.Label();
+            this.goalAmtTextBox = new System.Windows.Forms.TextBox();
+            this.yearsTextBox = new System.Windows.Forms.TextBox();
+            this.rateTextBox = new System.Windows.Forms.TextBox();
+            this.yearsLabel = new System.Windows.Forms.Label();
+            this.pcntLabel = new System.Windows.Forms.Label();
+            this.youNeedLabel = new System.Windows.Forms.Label();
+            this.resultDispLabel = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.dollarLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(47, 343);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(105, 23);
+            this.calculateButton.TabIndex = 0;
+            this.calculateButton.Text = "Calculate Amount";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(324, 343);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(84, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Present Value Calculator";
+            // 
+            // howMuchLabel
+            // 
+            this.howMuchLabel.Location = new System.Drawing.Point(56, 80);
+            this.howMuchLabel.Name = "howMuchLabel";
+            this.howMuchLabel.Size = new System.Drawing.Size(175, 46);
+            this.howMuchLabel.TabIndex = 3;
+            this.howMuchLabel.Text = "How much money do you want to have at the end of the period?";
+            // 
+            // howLongLabel
+            // 
+            this.howLongLabel.Location = new System.Drawing.Point(56, 126);
+            this.howLongLabel.Name = "howLongLabel";
+            this.howLongLabel.Size = new System.Drawing.Size(175, 39);
+            this.howLongLabel.TabIndex = 4;
+            this.howLongLabel.Text = "How many years are you willing to wait for interest to accrue?";
+            // 
+            // intRateLabel
+            // 
+            this.intRateLabel.AutoSize = true;
+            this.intRateLabel.Location = new System.Drawing.Point(56, 187);
+            this.intRateLabel.Name = "intRateLabel";
+            this.intRateLabel.Size = new System.Drawing.Size(122, 13);
+            this.intRateLabel.TabIndex = 5;
+            this.intRateLabel.Text = "What\'s the interest rate?";
+            // 
+            // goalAmtTextBox
+            // 
+            this.goalAmtTextBox.Location = new System.Drawing.Point(275, 80);
+            this.goalAmtTextBox.Name = "goalAmtTextBox";
+            this.goalAmtTextBox.Size = new System.Drawing.Size(100, 20);
+            this.goalAmtTextBox.TabIndex = 6;
+            this.goalAmtTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // yearsTextBox
+            // 
+            this.yearsTextBox.Location = new System.Drawing.Point(275, 126);
+            this.yearsTextBox.Name = "yearsTextBox";
+            this.yearsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.yearsTextBox.TabIndex = 7;
+            this.yearsTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // rateTextBox
+            // 
+            this.rateTextBox.Location = new System.Drawing.Point(275, 178);
+            this.rateTextBox.Name = "rateTextBox";
+            this.rateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.rateTextBox.TabIndex = 8;
+            this.rateTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // yearsLabel
+            // 
+            this.yearsLabel.AutoSize = true;
+            this.yearsLabel.Location = new System.Drawing.Point(381, 129);
+            this.yearsLabel.Name = "yearsLabel";
+            this.yearsLabel.Size = new System.Drawing.Size(32, 13);
+            this.yearsLabel.TabIndex = 9;
+            this.yearsLabel.Text = "years";
+            // 
+            // pcntLabel
+            // 
+            this.pcntLabel.AutoSize = true;
+            this.pcntLabel.Location = new System.Drawing.Point(381, 181);
+            this.pcntLabel.Name = "pcntLabel";
+            this.pcntLabel.Size = new System.Drawing.Size(40, 13);
+            this.pcntLabel.TabIndex = 10;
+            this.pcntLabel.Text = "% APR";
+            // 
+            // youNeedLabel
+            // 
+            this.youNeedLabel.AutoSize = true;
+            this.youNeedLabel.Location = new System.Drawing.Point(42, 249);
+            this.youNeedLabel.Name = "youNeedLabel";
+            this.youNeedLabel.Size = new System.Drawing.Size(379, 13);
+            this.youNeedLabel.TabIndex = 11;
+            this.youNeedLabel.Text = "Given the information provided, the amount you would need to deposit today is:";
+            // 
+            // resultDispLabel
+            // 
+            this.resultDispLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.resultDispLabel.Location = new System.Drawing.Point(159, 273);
+            this.resultDispLabel.Name = "resultDispLabel";
+            this.resultDispLabel.Size = new System.Drawing.Size(138, 29);
+            this.resultDispLabel.TabIndex = 12;
+            this.resultDispLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(201, 343);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 13;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // dollarLabel
+            // 
+            this.dollarLabel.AutoSize = true;
+            this.dollarLabel.Location = new System.Drawing.Point(256, 83);
+            this.dollarLabel.Name = "dollarLabel";
+            this.dollarLabel.Size = new System.Drawing.Size(13, 13);
+            this.dollarLabel.TabIndex = 14;
+            this.dollarLabel.Text = "$";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 400);
+            this.Controls.Add(this.dollarLabel);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.resultDispLabel);
+            this.Controls.Add(this.youNeedLabel);
+            this.Controls.Add(this.pcntLabel);
+            this.Controls.Add(this.yearsLabel);
+            this.Controls.Add(this.rateTextBox);
+            this.Controls.Add(this.yearsTextBox);
+            this.Controls.Add(this.goalAmtTextBox);
+            this.Controls.Add(this.intRateLabel);
+            this.Controls.Add(this.howLongLabel);
+            this.Controls.Add(this.howMuchLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calculateButton);
+            this.Name = "Form1";
+            this.Text = "Present Value Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label howMuchLabel;
+        private System.Windows.Forms.Label howLongLabel;
+        private System.Windows.Forms.Label intRateLabel;
+        private System.Windows.Forms.TextBox goalAmtTextBox;
+        private System.Windows.Forms.TextBox yearsTextBox;
+        private System.Windows.Forms.TextBox rateTextBox;
+        private System.Windows.Forms.Label yearsLabel;
+        private System.Windows.Forms.Label pcntLabel;
+        private System.Windows.Forms.Label youNeedLabel;
+        private System.Windows.Forms.Label resultDispLabel;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label dollarLabel;
+    }
+}
+
---
+++ Present Value Calculator/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/* 
+ Spencer Henze
+ ITM 225
+ 10/10/16
+ */
+namespace Present_Value_Calculator
+{
+    public partial class Form1 : Form
+    {
+        double pV, fV, enteredRate, rate, years;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Begin Declaring Methods:
+
+        private void GetValues() // this method simply calls the other methods to consolidate the fetching process and will be used later when the calculate button is clicked for concise logic.
+        {
+            //fV = double.Parse(goalAmtTextBox.Text);
+            fV = GetFV();
+            years = GetYears();
+            enteredRate = GetEnteredRate();
+            rate = ConvertRate();
+            
+        } // end GetValues() method
+
+        private double GetFV() // this method fetches the fV from the user, validates that it is a number entry, then parses the value to the fV variable.
+        {
+            if (double.TryParse(goalAmtTextBox.Text, out fV))
+            {
+                return fV;
+            }
+            else // if a non-numerical entry is entered, this will throw an error, set the focus to the textbox that was incorrect, clear its value, and return a dummy value to be validated later.
+            {
+                MessageBox.Show("Please enter a numerical value in the first text box", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                goalAmtTextBox.Focus();
+                goalAmtTextBox.Text = "";
+                fV = 0;         // these last two lines are important to keep the method happy, and to validate the entry when calculating pV.
+                return fV;
+            }
+        } // end GetfV() method
+
+        private double GetYears() // this method fetches the years value from the user, validates that it is a number entry, then parses the value to the years variable.
+        {
+            if (double.TryParse(yearsTextBox.Text, out years))
+            {
+                return years;
+            }
+            else
+            {
+                MessageBox.Show("Please enter a numerical value in the second text box", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                yearsTextBox.Focus();
+                yearsTextBox.Text = "";
+                years = 0;     // these last two lines are important to keep the method happy, and to validate the entry when calculating pV.
+                return years;  
+            }
+        } // end GetYears() method.
+
+        private double GetEnteredRate() // this method fetches the intermediate entered rate from the user, validates that it is a number entry, then parses the value to the intermediate variable called "enteredRate".
+        {
+            if (double.TryParse(rateTextBox.Text, out enteredRate)) //validates that numbers were entered
+            {
+                return enteredRate;
+            }
+            else
+            {
+                MessageBox.Show("Please enter a numerical value in the third text box. \nAlso, make sure your rate is annualized!", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                rateTextBox.Focus();
+                rateTextBox.Text = "";
+                enteredRate = 0;     // these last two lines are important to keep the method happy, and to validate the entry when calculating pV.
+                return enteredRate;
+            }
+        } // end GetEnteredRate() method
+
+
+        private double ConvertRate() // this method converts the rate entered by the user, or at this point the "enteredRate" variable, to a decimal to be used in the calculation
+        {
+            rate = enteredRate / 100;
+            return rate;
+        } // end ConvertRate() method
+
+        private void CalculatePV() // this method calculates the PV
+        {
+            double inside = 1 + rate; // I don't know of a way to use an exponential operator, so I first calculate the exponential part by calculating what is inside the parenthesis so that the Math.Pow() method can run the exponential process on a single perameter.
+            double denom = Math.Pow(inside, years); // this finishes calculating the denominator of the equation.
+            pV = fV / denom; // now that we have a concise denominator, we can do a simple division.
+        } //end CalculatePV() method
+
+        private void ClearFields() // I made this a method of its own so that I could just call it in other methods.
+        {
+            goalAmtTextBox.Text = "";
+            yearsTextBox.Text = "";
+            rateTextBox.Text = "";
+            resultDispLabel.Text = "";
+        } // end ClearFields() method
+
+        //begin click events:
+
+        private void calculateButton_Click(object sender, EventArgs e) // Here's the meat and potatoes.
+        {
+            GetValues();
+            CalculatePV();
+
+            if (pV != 0 && years != 0 && enteredRate != 0) // this is to validate that no errors were thrown. If there was an error, one of these three variables will have been set to 0 and the result will not display.
+            {
+                resultDispLabel.Text = pV.ToString("C"); // display the result of the calculation by converting it to a string and formatting it as currency
+            }
+
+        } // end calculateButton Click event
+
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        } // end exit button click event
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            ClearFields(); // uses the ClearFields() method to clear all of the fields.
+        }  // End clear button click event
+
+    } // end class Form1
+} // end Namespace Present_Value_Calculator
